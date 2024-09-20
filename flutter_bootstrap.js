fed101f4de4a156b8ba8f0ed9d0bdbbca7126154
Michelle Dayangco <dayangcomichelle0503@gmail.com>
2024-09-20 23:04:55 +0800

    Update for latest changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1174251235"
+    serviceWorkerVersion: "2662612376"
   }
 });
